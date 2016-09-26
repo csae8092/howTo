5dd678f666a358d86a02f8cc5f8d20a86cb054e5
Peter Andorfer <Peter.Andorfer@oeaw.ac.at>
2016-09-26 11:15:14 +0200

    added dynamic url pattern to project urls so images will be served during development and in production

--- howto/urls.py
+++ howto/urls.py
@@ -1,5 +1,7 @@
 from django.conf.urls import url, include
 from django.contrib import admin
+from django.conf.urls.static import static
+from django.conf import settings
 
 urlpatterns = [
     url(r'^admin/', admin.site.urls),
@@ -7,3 +9,6 @@ urlpatterns = [
     url(r'^account/', include('account.urls', namespace='account')),
     url(r'^blog/', include('blog.urls', namespace='dynamicblog'))
 ]
+
+if settings.DEBUG is True:
+    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)

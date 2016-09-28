001febba739950ea0003072f3dc156b77407f9a0
Peter Andorfer <Peter.Andorfer@oeaw.ac.at>
2016-09-28 18:20:51 +0200

    set link to imprint and changed base temmplate for login/logout temmplates

--- webpage/metadata.py
+++ webpage/metadata.py
@@ -5,7 +5,7 @@ PROJECT_METADATA = {
     'title': 'HowTo',
     'author': 'Peter Andorfer',
     'subtitle': 'A blog',
-    'description': 'This blog tries to share knowledge about how to do things, mainly concerning software developement and best practices in the Digital Huminites.',
+    'description': 'This blog tries to share knowledge about how to do things, mainly concerning software developement and best practices in the Digital Humanities.',
     'github': 'https://github.com/csae8092/howto',
     'purpose_de': 'Das Ziel des Projekts "howto" ist die Publikation von Blog Posts zum Thema Digital Humanities.',
     'purpose_en': 'The purpose of the project "howto" is the publication blog posts about Digital Humanities.'}
